ba5deefc205fbadb3e5300c553421a70706cef36
dacianG <dacian.goina00@e-uvt.ro>
2021-11-05 18:12:32 +0200

    carousel and header

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import { showMessageFromBackend } from "./services/example";
 import BootstrapComponent from "./components/bootstrap-component";
 import NavBar from "./components/nav-bar";
+import HomeCarousel from "./components/home_carousel";
 
 const App = () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    showMessageFromBackend().then((res) => setMessages(res.data));
+    //showMessageFromBackend().then((res) => setMessages(res.data));
   }, []);
 
   const navBarItems = ["Home Page", "Services", "Login", "About Us"];
@@ -18,6 +19,10 @@ const App = () => {
       {messages.map((message) => (
         <div key={message.id}>{message.field}</div>
       ))}
+
+        <HomeCarousel />
+
+
       <BootstrapComponent />
     </>
   );
